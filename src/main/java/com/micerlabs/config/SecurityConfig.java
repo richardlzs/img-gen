ee51e70e61c4f0571bcda49ec8af0f6e5268ecf4
bwx <1131188047@qq.com>
2023-06-21 17:06:04 +0800

    最新的html。D:\img-gen\web文件夹也拉了进来，记得更新一下

--- src/main/java/com/micerlabs/config/SecurityConfig.java
+++ src/main/java/com/micerlabs/config/SecurityConfig.java
@@ -23,10 +23,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //authorizeRequests开启权限配置
-        http.authorizeRequests()
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("test01").password("{noop}123456+").authorities("test").build());
+        manager.createUser(User.withUsername("test222").password("{noop}123456+").authorities("test").build());
+        http
+                .userDetailsService(manager)
+                .authorizeRequests()
                 //anyRequest().authenticated()所有请求都要认证
-                .requestMatchers(new RegexRequestMatcher(".*","GET")).authenticated()
-                .requestMatchers(new RegexRequestMatcher(".*","POST")).authenticated()
+                .requestMatchers(new RegexRequestMatcher(".*", "GET")).authenticated()
+                .requestMatchers(new RegexRequestMatcher(".*", "POST")).authenticated()
                 //.anyRequest().authenticated()
                 //and()和http同意
                 .and()
@@ -37,26 +42,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                //defaultSuccessUrl登录成功跳转的页面,如果直接到Login页面登陆成功跳转的地址
                 .defaultSuccessUrl("/h5.html")
                 .successHandler((httpServletRequest, httpServletResponse, authentication) -> httpServletResponse.sendRedirect("h5.html"))
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-                        Principal p=httpServletRequest.getUserPrincipal();
-                    }
-                })
                 //登录的用户名
                 .usernameParameter("uname")
                 //登录的密码
                 .passwordParameter("passwd")
-
                 //表示跟登录相关的接口不拦截
                 .permitAll()
                 .and()
                 //关闭csrf跨域攻击
                 .csrf().disable();
 
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("test01").password("123456+").authorities("test").build());
-        manager.createUser(User.withUsername("test222").password("123456+").authorities("test").build());
-        http.userDetailsService(manager);
     }
 }
